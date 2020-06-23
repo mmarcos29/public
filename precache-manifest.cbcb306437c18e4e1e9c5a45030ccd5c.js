a841b625abf696eb5754b6eaab2f697964559577
mmarcos29 <joaomarcos1916@gmail.com>
2020-06-22 23:49:07 -0300

    Updates

---
+++ precache-manifest.cbcb306437c18e4e1e9c5a45030ccd5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a1b7ffbcc845e41163e7422f63ea58",
+    "url": "/public/index.html"
+  },
+  {
+    "revision": "beadc4cbc1cfcaeb22c4",
+    "url": "/public/static/css/main.470a7617.chunk.css"
+  },
+  {
+    "revision": "ab6ba11b03b5155e935e",
+    "url": "/public/static/js/2.c08c2013.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/public/static/js/2.c08c2013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beadc4cbc1cfcaeb22c4",
+    "url": "/public/static/js/main.d08d2fb1.chunk.js"
+  },
+  {
+    "revision": "61ab95e27b327dd533da",
+    "url": "/public/static/js/runtime-main.68726571.js"
+  },
+  {
+    "revision": "a6a68097072203a4bc2ab17290a875b3",
+    "url": "/public/static/media/logo-revolution.a6a68097.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04293d791d99b70b5ed5e811fc06beda.js"
+  "/public/precache-manifest.cbcb306437c18e4e1e9c5a45030ccd5c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/public/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

5543366c5366a4053ec555eb1bc7131a1823bd78
mmarcos29 <joaomarcos1916@gmail.com>
2020-06-22 23:44:59 -0300

    Updates

---
+++ precache-manifest.04293d791d99b70b5ed5e811fc06beda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b17175570e05a38661db357a804f7c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "beadc4cbc1cfcaeb22c4",
+    "url": "/static/css/main.470a7617.chunk.css"
+  },
+  {
+    "revision": "ab6ba11b03b5155e935e",
+    "url": "/static/js/2.c08c2013.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c08c2013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beadc4cbc1cfcaeb22c4",
+    "url": "/static/js/main.d08d2fb1.chunk.js"
+  },
+  {
+    "revision": "8bcedf9ca0e068e3486a",
+    "url": "/static/js/runtime-main.7f01eb7f.js"
+  },
+  {
+    "revision": "a6a68097072203a4bc2ab17290a875b3",
+    "url": "/static/media/logo-revolution.a6a68097.png"
+  }
+]);

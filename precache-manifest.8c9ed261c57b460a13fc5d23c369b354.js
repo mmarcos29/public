1149a4203757bafb79eae1938b6053b58033b1a5
mmarcos29 <joaomarcos1916@gmail.com>
2020-08-11 14:39:32 -0300

    Updates

---
+++ precache-manifest.8c9ed261c57b460a13fc5d23c369b354.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b900eda8b16dde7dcde25bbe893887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "871580d4f117f0945e87",
+    "url": "/static/css/main.004f6cf0.chunk.css"
+  },
+  {
+    "revision": "d3083202939ff274b690",
+    "url": "/static/js/2.8e3c5817.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8e3c5817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "871580d4f117f0945e87",
+    "url": "/static/js/main.70be887d.chunk.js"
+  },
+  {
+    "revision": "8bcedf9ca0e068e3486a",
+    "url": "/static/js/runtime-main.7f01eb7f.js"
+  },
+  {
+    "revision": "a6a68097072203a4bc2ab17290a875b3",
+    "url": "/static/media/logo-revolution.a6a68097.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/public/precache-manifest.30d699a2a72528338a69b11035ab66c4.js"
+  "/precache-manifest.8c9ed261c57b460a13fc5d23c369b354.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/public/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
